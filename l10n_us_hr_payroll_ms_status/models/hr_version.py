a933734cc6700e0fcee309e3d66c15c68793e716
CrossKittrell-Titan <270380784+CrossKittrell-Titan@users.noreply.github.com>
2026-07-30 01:22:47 -0500

    Expose Mississippi statuses in payroll field metadata

--- l10n_us_hr_payroll_ms_status/models/hr_version.py
+++ l10n_us_hr_payroll_ms_status/models/hr_version.py
@@ -1,15 +1,17 @@
 from odoo import models
 
-from .mississippi_withholding import MS_FILING_STATUSES
+from .mississippi_withholding import with_ms_filing_statuses
 
 
 class HrVersion(models.Model):
     _inherit = "hr.version"
 
     def _get_selection_state_filing_status(self):
-        selection = list(super()._get_selection_state_filing_status())
-        existing = {value for value, _label in selection}
-        for value, config in MS_FILING_STATUSES.items():
-            if value not in existing:
-                selection.append((value, config["label"]))
-        return selection
+        return with_ms_filing_statuses(super()._get_selection_state_filing_status())
+
+    def fields_get(self, allfields=None, attributes=None):
+        result = super().fields_get(allfields=allfields, attributes=attributes)
+        field = result.get("l10n_us_state_filing_status")
+        if field and "selection" in field:
+            field["selection"] = with_ms_filing_statuses(field["selection"])
+        return result
